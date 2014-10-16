c3aa1e063975384373960982027a4defb8be58f8
Enoch Yip <chi-lok@hotmail.com>
2014-10-15 18:14:46 -0700

    services library with theoretically working CRUD functions

---
+++ WcfServiceLibrary1/WcfServiceLibrary1/IService1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+
+
+namespace WcfServiceLibrary1
+{
+    public struct User
+    {
+        public string UserId;
+        public string Username;
+        public string Email;
+        public string PasswordHash;
+    }
+
+    public struct Register
+    {
+        public string UserId;
+        public string Username;
+        public string Email;
+        public string PasswordHash;
+    }
+
+
+    public struct Login
+    {
+        public string PasswordHash;
+        public string Email;
+    }
+
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "WcfServiceLibrary1.ContractType".
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+
+    
+
+    [ServiceContract(Namespace = "http://techproproject.bcit.ca")]
+    public interface IService1
+    {
+        [OperationContract]
+        [WebInvoke(RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "users/{userid}")]
+        User getUser(int userid);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "users/register")]
+        void createUser(Register registrationData);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "users/{userid}")]
+        void updateUser(int userid, User user);
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "users/{userid}")]
+        void deleteUser(int userid);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "users/login")]
+        User login(Login loginData);
+    }
+}
---
+++ WcfServiceLibrary1/WcfServiceLibrary1/Service1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WcfServiceLibrary1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        User getUser(int userid)
+        {
+            // Create a connection to the "WeNomYou" SQL database located on the 
+            // local computer.
+            SqlConnection myConnection = new SqlConnection("Data Source=c106ta0dei.database.windows.net,1433\"SQLSERVER;Initial Catalog=WeNomYouDB;User ID=buklaudev;Password=Mirela#1");
+            // Connect to the SQL database using a SQL SELECT query to get all 
+            // the data from the "Titles" table.
+            SqlDataAdapter myCommand = new SqlDataAdapter("SELECT * " +
+               " FROM WeNomYou WHERE userid = \"" + userid + "\"" , myConnection);
+            // Create and fill a DataSet.
+            DataSet ds = new DataSet();
+            myCommand.Fill(ds);
+            //Create user and fill in properties
+            User user = new User();
+            user.UserId = ds.Tables[0].Rows[0]["ID"].ToString();
+            user.Username = ds.Tables[0].Rows[0]["name"].ToString();
+            user.PasswordHash = ds.Tables[0].Rows[0]["password"].ToString();
+            user.Email = ds.Tables[0].Rows[0]["email"].ToString();
+
+            // Select from users table where userid = userid
+            // userid, user email, password
+            return user;
+        }
+
+        void createUser(Register registrationData)
+        {
+            // Create a connection to the "WeNomYou" SQL database located on the server
+            SqlConnection myConnection = new SqlConnection("Data Source=c106ta0dei.database.windows.net,1433\"SQLSERVER;Initial Catalog=WeNomYouDB;User ID=buklaudev;Password=Mirela#1");
+            // Connect to the SQL database 
+            // Insert into users table
+            // userid, user email, password, email
+            SqlDataAdapter myCommand = new SqlDataAdapter("INSERT INTO WeNomYou (userId, username, password, email) VALUES ('" +
+                registrationData.UserId + "', '" +
+                registrationData.Username + "', '" +
+                registrationData.PasswordHash + "', '" +
+                registrationData.Email + "')" 
+                , myConnection);
+            
+        }
+
+        void updateUser(int userid, User user)
+        {
+            // Create a connection to the "WeNomYou" SQL database located on the server
+            SqlConnection myConnection = new SqlConnection("Data Source=c106ta0dei.database.windows.net,1433\"SQLSERVER;Initial Catalog=WeNomYouDB;User ID=buklaudev;Password=Mirela#1");
+            // Connect to the SQL database 
+            // Update users table where userid = user.userid
+            // userid, user email, password
+            SqlDataAdapter myCommand = new SqlDataAdapter("UPDATE WeNomYou SET username=\"" + user.Username
+                                                                        + "\", password=\"" + user.PasswordHash
+                                                                           + "\", email=\"" + user.Email
+                                                                     + "\" WHERE userid=\"" + user.UserId
+                                                                                            + "\"", myConnection);
+            
+        }
+
+        void deleteUser(int userid)
+        {
+            // Create a connection to the "WeNomYou" SQL database located on the 
+            // local computer.
+            SqlConnection myConnection = new SqlConnection("Data Source=c106ta0dei.database.windows.net,1433\"SQLSERVER;Initial Catalog=WeNomYouDB;User ID=buklaudev;Password=Mirela#1");
+            // Connect to the SQL database 
+            // Delete from users table where userid = user.userid
+            SqlDataAdapter myCommand = new SqlDataAdapter("DELETE * " +
+               " FROM WeNomYouDB WHERE userid = \"" + userid + "\"" , myConnection);
+            
+        }
+
+        User login(Login loginData)
+        {
+            // Create a connection to the "WeNomYou" SQL database located on the 
+            // local computer.
+            SqlConnection myConnection = new SqlConnection("Data Source=c106ta0dei.database.windows.net,1433\"SQLSERVER;Initial Catalog=WeNomYouDB;User ID=buklaudev;Password=Mirela#1");
+            // Connect to the SQL database using a SQL SELECT query to get all 
+            // the data from the "Titles" table.
+            SqlDataAdapter myCommand = new SqlDataAdapter("SELECT * " +
+               " FROM WeNomYou WHERE password = \"" + loginData.PasswordHash + "\" && email = \"" + loginData.Email + "\"" , myConnection);
+            // Create and fill a DataSet.
+            DataSet ds = new DataSet();
+            myCommand.Fill(ds);
+            //Create user and fill in properties
+            User user = new User();
+            user.PasswordHash = ds.Tables[0].Rows[0]["password"].ToString();
+            user.Email = ds.Tables[0].Rows[0]["email"].ToString();
+
+            // Receives email(?) and hash of password (SHA256?)
+            // Select from users table where passwordhash = loginData.passwordhash
+            return user;
+        }
+    }
+}
